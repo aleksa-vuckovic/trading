Fix 0 shares division, price normalization

trading/models/model1/example.py
@@ -1,4 +1,4 @@
-from ...utils import dateutils
+from ...utils import dateutils, common
 from ...data import aggregate, nasdaq
 import random
 import time
@@ -65,7 +65,8 @@ class PriceTarget(Enum):
             return (1-x)/(1+x)
         raise Exception("Unknown price target type")
     
-    def plot(self):
+    @staticmethod
+    def plot():
         x = torch.linspace(-0.15, 0.15, 100, dtype=torch.float32)
         for i, pt in enumerate(PriceTarget):
             fig = plt.figure(i // 4)
@@ -97,17 +98,15 @@ def generate_input(
     d1_volumes = torch.tensor(d1_volumes[-D1_PRICES:], dtype=torch.float64)
     h1_prices = torch.tensor(h1_prices[-H1_PRICES:], dtype=torch.float64)
     h1_volumes = torch.tensor(h1_volumes[-H1_PRICES:], dtype=torch.float64)
-
-    #2. Normalize prices and volumes
-    last_price = float(d1_prices[-1])
-    d1_prices /= last_price
-    h1_prices /= last_price
-
-    shares = aggregate.get_shares_outstanding_at(ticker, end_time)
-    d1_volumes /= shares
-    h1_volumes /= shares
-    market_cap = torch.tensor([last_price*shares], dtype=torch.float64)
+    last_price = float(h1_prices[-1])
+    market_cap = torch.tensor([last_price*aggregate.get_shares_outstanding_at(ticker, end_time)], dtype=torch.float64)
     
+    #2. Normalize prices and volumes
+    common.normalize_in_place(d1_prices)
+    common.normalize_in_place(d1_volumes)
+    common.normalize_in_place(h1_prices)
+    common.normalize_in_place(h1_volumes)
+
     #3. Get the textual data
     market_summary = aggregate.get_market_summary(end_time)
     company_summary = aggregate.get_company_summary(ticker)
@@ -119,7 +118,11 @@ def generate_input(
             output = model(**inputs).pooler_output.flatten()
         text_inputs.append(output)
     
-    return torch.cat([d1_prices, d1_volumes, h1_prices, h1_volumes, *text_inputs, market_cap], dim=0), last_price
+    result = torch.cat([d1_prices, d1_volumes, h1_prices, h1_volumes, *text_inputs, market_cap], dim=0)
+    bad_entries = torch.logical_or(result.isnan(), result.isinf()).sum().item()
+    if bad_entries > 0:
+        raise Exception(f"Generated example has {bad_entries} nans or infs.")
+    return result, last_price
 
 def generate_example(
     ticker: nasdaq.NasdaqListedEntry,

trading/models/model1/network.py
@@ -1,6 +1,9 @@
 import torch
 from . import example
 import math
+import torchinfo
+from . import generator
+import config
 
 
 class IndividualTextLayer(torch.nn.Module):
@@ -115,13 +118,15 @@ class IndividualSeriesLayer(torch.nn.Module):
     """
     def __init__(self, input_length: int, output_features: int = 100):
         super().__init__()
-        self.conv1 = torch.nn.Sequential(
-            torch.nn.Conv1d(in_channels = 1, out_channels = 5, kernel_size = 4, stride = 1),
-            torch.nn.LeakyReLU(),
-            torch.nn.Conv1d(in_channels = 5, out_channels = 10, kernel_size = 5, stride = 1),
-            torch.nn.LeakyReLU(),
-            torch.nn.BatchNorm1d(num_features=10)
-        )
+        #self.conv1 = torch.nn.Sequential(
+        self.batch1 = torch.nn.BatchNorm1d(num_features=1)
+        self.conv1=    torch.nn.Conv1d(in_channels = 1, out_channels = 5, kernel_size = 4, stride = 1)
+        self.relu1=    torch.nn.LeakyReLU()
+        self.batch2 = torch.nn.BatchNorm1d(num_features=5)
+        self.conv12=    torch.nn.Conv1d(in_channels = 5, out_channels = 10, kernel_size = 5, stride = 1)
+        self.relu2=    torch.nn.LeakyReLU()
+        self.batch3=    torch.nn.BatchNorm1d(num_features=10)
+        #)
         inner_features = input_length-3-4
 
         self.conv2 = torch.nn.Sequential(
@@ -131,7 +136,7 @@ class IndividualSeriesLayer(torch.nn.Module):
             torch.nn.AvgPool1d(kernel_size=5),
             torch.nn.Conv1d(in_channels=10, out_channels=15, kernel_size=6, stride=2),
             torch.nn.LeakyReLU(),
-            torch.nn.BatchNorm1d(num_features=15)
+            torch.nn.BatchNorm1d(num_features=15, eps=0.0001),
         )
         outer_features = ((inner_features//5-5)//5-6)//2+1
 
@@ -144,7 +149,17 @@ class IndividualSeriesLayer(torch.nn.Module):
 
     def forward(self, series):
         series = torch.unsqueeze(series, dim = 1)
-        inner = self.conv1(series)
+        nans = series.isnan().sum()
+        #inner = self.conv1(series)
+        torch.save(series, 'test2.pt')
+        inner = self.batch1(series)
+        inner = self.conv1(inner)
+        inner = self.relu1(inner)
+        inner = self.batch2(inner)
+        inner = self.conv12(inner)
+        inner = self.relu2(inner)
+        torch.save(inner, 'TEST.pt')
+        inner = self.batch3(inner)
         outer = self.dense(torch.flatten(self.conv2(inner), start_dim=1))
         inner = torch.flatten(inner, start_dim=1)
         return torch.concat([inner[:,-self.concat_features:], outer], dim=1)
@@ -171,10 +186,12 @@ class Model(torch.nn.Module):
         self.series_layer = SeriesLayer(output_features=series_features)
 
         combined_features = final_text_features + series_features*4
-        self.dense1 = torch.nn.Sequential(
-            torch.nn.Linear(in_features=combined_features, out_features=100),
-            torch.nn.Tanh()
-        )
+        #self.dense1 = torch.nn.Sequential(
+        #    torch.nn.Linear(in_features=combined_features, out_features=100),
+        #    torch.nn.Tanh()
+        #)
+        self.lin1 = torch.nn.Linear(in_features=combined_features, out_features=100)
+        self.tanh1 = torch.nn.Tanh()
         self.dense2 = torch.nn.Sequential(
             torch.nn.Linear(in_features=100, out_features=10),
             torch.nn.Tanh()
@@ -187,7 +204,21 @@ class Model(torch.nn.Module):
         text_out = self.text_layer(text1, text2, text3)
         series_out = self.series_layer(series1, series2, series3, series4)
         combined = torch.concat([series_out, text_out], dim = 1)
-        return self.dense3(self.dense2(self.dense1(combined)))
+        #res = self.dense1(combined)
+        res = self.lin1(combined)
+        res = self.tanh1(res)
+        res = self.dense2(res)
+        res = self.dense3(res)
+        return res
+    
+    @staticmethod
+    def print_summary():
+        model = Model()
+        input = [(config.batch_size, example.D1_PRICES)]*2
+        input += [(config.batch_size, example.H1_PRICES)]*2
+        input += [(config.batch_size, example.TEXT_EMBEDDING_SIZE)]*3
+        torchinfo.summary(model, input_size=input)
+
     
 """64 layers * 100 features * 8 bytes ~ 50kB
     50kB * 5000 examples = 250MB

trading/utils/test_common.py
@@ -4,8 +4,29 @@ import time
 from pathlib import Path
 import shutil
 import json
+import torch
 
 class TestCommon(unittest.TestCase):
+
+    def test_normalize_in_place(self):
+        tensor = torch.tensor([[1,2,3,4],[5,6,7,8]], dtype=torch.float32)
+        expect = torch.tensor([[1,2/3,1,4],[5,6/7,1,8]], dtype=torch.float32)
+        maxes = common.normalize_in_place(tensor, 1, 2, dim=1)
+        self.assertEqual(8, (tensor==expect).sum().item())
+        self.assertEqual((2,1), tuple(maxes.shape))
+
+        tensor = torch.tensor([[1,2,torch.nan,4],[5,torch.inf,7,8]], dtype=torch.float32)
+        expect = torch.tensor([[1,torch.nan,torch.nan,4],[5,torch.nan,0,8]], dtype=torch.float32)
+        common.normalize_in_place(tensor, 1,2, dim=1)
+        self.assertEqual(5, (tensor==expect).sum().item())
+        self.assertEqual(expect.isnan().sum().item(), tensor.isnan().sum().item())
+
+        tensor = torch.tensor([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]], dtype=torch.float32)
+        expect = torch.tensor([[[1/7,2/8,3/9],[4/10,5/11,6/12]],[[1,1,1],[1,1,1]]], dtype=torch.float32)
+        maxes = common.normalize_in_place(tensor)
+        self.assertEqual(12, (tensor==expect).sum().item())
+        self.assertEqual((1,2,3), tuple(maxes.shape))
+
     def test_binary_search(self):
         collection = [
             {"time": 1},
@@ -32,7 +53,7 @@ class TestCommon(unittest.TestCase):
         base_timeout = 0.05
         #create a method that always throws an exception
         invocations = 0
-        @common.backup_timeout(exc_type=TestException, behavior=common.BackupBehavior.NONE, base_timeout=base_timeout, backoff_factor=2)
+        @common.backup_timeout(exc_type=TestException, behavior=common.BackupBehavior.DEFAULT, base_timeout=base_timeout, backoff_factor=2)
         def test_method():
             nonlocal invocations
             invocations += 1