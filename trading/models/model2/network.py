1384d924d688a18737f7e23afdfe25cb41f800d4
Aleksa <aleksa.vuckovic36@gmail.com>
2025-02-02 12:33:10 +0100

    Start model2

---
+++ trading/models/model2/network.py
@@ -0,0 +1,115 @@
+import torch
+from . import example
+import math
+import torchinfo
+from . import generator
+import config
+
+
+class IndividualSeriesLayer(torch.nn.Module):
+    """
+    10 layer, 12+90+350=452...
+    ----------------------------
+    1 conv+relu, 1*x->5*(x-3)
+    1 conv+relu, 5*(x-3)->10*(x-7)
+    --
+    1 avgpool,   10*(x-7)->10*((x-7)//5)
+    1 conv+relu,  10*t->10*(t-5)
+    1 avgpool,   10*(t-5)->10*((t-5)//5)
+    1 conv+relu, 10-
+    """
+    def __init__(self, input_length: int, output_features: int = 100):
+        super().__init__()
+        self.conv1 = torch.nn.Sequential(
+            torch.nn.Conv1d(in_channels = 1, out_channels = 5, kernel_size = 4, stride = 1),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(num_features=5),
+            torch.nn.Conv1d(in_channels = 5, out_channels = 10, kernel_size = 5, stride = 1),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(num_features=10)
+        )
+        inner_features = input_length-3-4
+
+        self.conv2 = torch.nn.Sequential(
+            torch.nn.AvgPool1d(kernel_size=5),
+            torch.nn.Conv1d(in_channels = 10, out_channels = 10, kernel_size = 6, stride = 1),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(num_features=10),
+            torch.nn.AvgPool1d(kernel_size=5),
+            torch.nn.Conv1d(in_channels=10, out_channels=15, kernel_size=6, stride=2),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(num_features=15, eps=0.0001),
+        )
+        outer_features = ((inner_features//5-5)//5-6)//2+1
+
+        self.concat_features = math.ceil(output_features/2)
+        self.dense = torch.nn.Sequential(
+            torch.nn.Linear(in_features=outer_features*15, out_features=output_features//2),
+            torch.nn.Tanh()
+        )
+
+    def forward(self, series):
+        series = torch.unsqueeze(series, dim = 1)
+        inner = self.conv1(series)
+        outer = self.dense(torch.flatten(self.conv2(inner), start_dim=1))
+        inner = torch.flatten(inner, start_dim=1)
+        return torch.concat([inner[:,-self.concat_features:], outer], dim=1)
+
+class SeriesLayer(torch.nn.Module):
+    """40 layers"""
+    def __init__(self, output_features: int = 100):
+        super().__init__()
+        self.individual_layers = torch.nn.ModuleList([
+            IndividualSeriesLayer(input_length=example.D1_PRICES if i < 2 else example.H1_PRICES, output_features=output_features)
+            for i in range(4)
+        ])
+    
+    def forward(self, series1, series2, series3, series4):
+        series = [series1, series2, series3, series4]
+        results = [self.individual_layers[i](series[i]) for i in range(4)]
+        return torch.concat(results, dim=1)
+
+class Model(torch.nn.Module):
+    """64 layers"""
+    def __init__(self, individual_text_features: int = 40, final_text_features: int = 100, series_features: int = 100):
+        super().__init__()
+        self.text_layer = TextLayer(individual_features=individual_text_features, out_features=final_text_features)
+        self.series_layer = SeriesLayer(output_features=series_features)
+
+        combined_features = final_text_features + series_features*4
+        self.dense1 = torch.nn.Sequential(
+            torch.nn.Linear(in_features=combined_features, out_features=100),
+            torch.nn.Tanh()
+        )
+        self.dense2 = torch.nn.Sequential(
+            torch.nn.Linear(in_features=100, out_features=10),
+            torch.nn.Tanh()
+        )
+        self.dense3 = torch.nn.Sequential(
+            torch.nn.Linear(in_features=10, out_features=1),
+            torch.nn.Tanh()
+        )
+    def forward(self, series1, series2, series3, series4, text1, text2, text3):
+        text_out = self.text_layer(text1, text2, text3)
+        series_out = self.series_layer(series1, series2, series3, series4)
+        combined = torch.concat([series_out, text_out], dim = 1)
+        res = self.dense1(combined)
+        res = self.dense2(res)
+        res = self.dense3(res)
+        return res
+    
+    @staticmethod
+    def print_summary():
+        model = Model()
+        input = [(config.batch_size, example.D1_PRICES)]*2
+        input += [(config.batch_size, example.H1_PRICES)]*2
+        input += [(config.batch_size, example.TEXT_EMBEDDING_SIZE)]*3
+        torchinfo.summary(model, input_size=input)
+
+    
+"""64 layers * 100 features * 8 bytes ~ 50kB
+    50kB * 5000 examples = 250MB
+
+    Let's keep it 1000 examples per batch
+
+"""
---
+++ trading/models/model2/train.py
@@ -0,0 +1,162 @@
+from .network import Model
+import os
+from pathlib import Path
+import re
+import torch
+from torch import optim
+from tqdm import tqdm
+from . import example
+import logging
+from ...utils import common
+
+logger = logging.getLogger(__name__)
+#Using 1/6th for validation
+examples_folder = Path(__file__).parent / 'examples'
+checkpoint_file = Path(__file__).parent / 'checkpoint.pth'
+special_checkpoint_file = Path(__file__).parent / 'special_checkpoint.pth'
+learning_rate = 10e-7
+
+class Accuracy(common.StatCollector):
+    def __init__(self):
+        super().__init__('accuracy')
+    
+    def _calculate(self, expect, output):
+        output = output > 0.5
+        expect = expect > 0.5
+        hits = torch.logical_and(output, expect).sum().item()
+        output_n = output.sum().item()
+        expect_n = expect.sum().item()
+        return hits / output_n if output_n else 0 if expect_n else 1
+    
+class Precision(common.StatCollector):
+    def __init__(self):
+        super().__init__('precision')
+
+    def _calculate(self, expect, output):
+        output = output > 0.5
+        expect = expect > 0.5
+        hits = torch.logical_and(output, expect).sum().item()
+        expect_n = expect.sum().item()
+        return hits / expect_n if expect_n else 1
+
+class Miss(common.StatCollector):
+    def __init__(self):
+        super().__init__('miss')
+    
+    def _calculate(self, expect, output):
+        output = output > 0.2
+        misses_n = torch.logical_and(expect < 0, output).sum().item()
+        total_n = output.sum().item()
+        return misses_n / total_n if total_n else 0
+    
+class CustomLoss(common.StatCollector):
+    def __init__(self):
+        super().__init__('loss')
+    
+    def _calculate(self, expect, output):
+        eps = 1e-5
+        loss = -torch.log(1 + eps - torch.abs(output - expect) / (1+torch.abs(expect)))
+        return loss.mean()
+    
+def create_stats(name: str) -> common.StatContainer:
+    return common.StatContainer(CustomLoss(), Accuracy(), Precision(), Miss(), name=name)
+
+
+def get_all_files() -> list[dict]:
+    pattern = re.compile(r"([^_]+)_batch(\d+)-(\d+).pt")
+    files = [ pattern.fullmatch(it) for it in os.listdir(examples_folder)]
+    files = [ {'file': it.group(0), 'source': it.group(1), 'batch': int(it.group(2)), 'hour': int(it.group(3))} for it in files if it ]
+    return sorted(files, key=lambda it: (it['source'], it['hour'], it['batch']))
+all_files = get_all_files()
+
+def get_training_files() -> list[str]:
+    return [it for it in all_files if it['batch'] % 6]
+
+def get_validation_files() -> list[str]:
+    return [it for it in all_files if it['batch']%6 == 0]
+
+def run_loop(max_epochs = 100000000):
+    training_files = get_training_files()
+    validation_files = get_validation_files()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logger.info(f"Loaded {len(training_files)} training and {len(validation_files)} validation batches.")
+    logger.info(f"Device: {device}")
+    model = Model().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    train_stats = create_stats('train')
+    val_stats = create_stats('val')
+
+    if checkpoint_file.exists():
+        data = torch.load(checkpoint_file, weights_only=True, map_location=device)
+        model.load_state_dict(data['model_state_dict'])
+        optimizer.load_state_dict(data['optimizer_state_dict'])
+        epoch = data['epoch'] + 1
+        history = data['history']
+        logger.info(f"Restored state from epoch {epoch-1}")
+    else:
+        logger.info(f"No state, starting from scratch.")
+        epoch = 1
+        history = []
+
+    def extract_tensors(batch: torch.Tensor) -> torch.Tensor:
+        series1 = batch[:,example.D1_PRICES_I:example.D1_PRICES_I+example.D1_PRICES]
+        series2 = batch[:,example.D1_VOLUMES_I:example.D1_VOLUMES_I+example.D1_PRICES]
+        series3 = batch[:,example.H1_PRICES_I:example.H1_PRICES_I+example.H1_PRICES]
+        series4 = batch[:,example.H1_VOLUMES_I:example.H1_VOLUMES_I+example.H1_PRICES]
+        text1 = batch[:,example.TEXT1_I:example.TEXT1_I+example.TEXT_EMBEDDING_SIZE]
+        text2 = batch[:,example.TEXT2_I:example.TEXT2_I+example.TEXT_EMBEDDING_SIZE]
+        text3 = batch[:,example.TEXT3_I:example.TEXT3_I+example.TEXT_EMBEDDING_SIZE]
+        expect = batch[:,example.D1_TARGET_I]
+        expect = example.PriceTarget.TANH_10_10.get_price(expect)
+        return series1, series2, series3, series4, text1, text2, text3, expect
+    
+    while epoch < max_epochs:
+        model.train()
+        with tqdm(training_files, desc=f"Epoch {epoch}", leave=True) as bar:
+            for item in bar:
+                batch = torch.load(examples_folder / item['file'], weights_only=True).to(device, dtype=torch.float32)
+                logger.info(f"Loaded a batch of shape {batch.shape}")
+                tensors = extract_tensors(batch)
+                input = tensors[:-1]
+                expect = tensors[-1]
+
+                optimizer.zero_grad()
+                output = model(*input).squeeze()
+                loss = train_stats.update(expect, output)
+                loss.backward()
+                optimizer.step()
+                
+                bar.set_postfix_str(str(train_stats))
+        
+        model.eval()
+        with torch.no_grad():
+            with tqdm(validation_files, desc = 'Validation...', leave=False) as bar:
+                for item in bar:
+                    batch = torch.load(examples_folder / item['file'], weights_only=True).to(device, dtype=torch.float32)
+                    tensors = extract_tensors(batch)
+                    input = tensors[:-1]
+                    expect = tensors[-1]
+
+                    output = model(*input).squeeze()
+                    val_stats.update(expect, output)
+                    
+        print(str(val_stats))
+        history.append({**train_stats.to_dict(), **val_stats.to_dict(), 'epoch': epoch})
+        train_stats.clear()
+        val_stats.clear()
+        epoch += 1
+
+        savedict = {
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'epoch': epoch,
+            'history': history
+        }
+        torch.save(savedict, checkpoint_file)
+
+            
+
+
+
+                
+                
