733c10ab1e3161bcd66be941253755ea652e9e46
Aleksa <aleksa.vuckovic36@gmail.com>
2025-02-27 03:45:32 +0100

    Add another generator configuration

--- storage.py
+++ storage.py
@@ -23,3 +23,23 @@ class generator:
         .build()\
         .for_interval(Interval.M5)
     folder = Path(__file__).parent/"trading"/"models"/"generators"/"examples_w1_m5"
+
+class generator2:
+    data_config = DataConfig({
+        Interval.W1: 50,
+        Interval.D1: 150,
+        Interval.H1: 150,
+        Interval.M15: 200
+    })
+    after_data_config = DataConfig({
+        Interval.D1: 5,
+        Interval.H1: 2*7,
+        Interval.M15: 2*7*4,
+        Interval.M5: 2*7*12
+    })
+    timing = TimingConfig.Builder()\
+        .starting(hour = 10, minute = 0)\
+        .until(hour = 15, minute=30)\
+        .build()\
+        .for_interval(Interval.M15)
+    folder = Path(__file__).parent/"trading"/"models"/"generators"/"examples_w1_m15"
