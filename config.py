08d42ab69541262d4586c76d27e34528ffd6db1a
Aleksa Vuckovic <aleksa.vuckovic36@gmail.com>
2025-05-17 17:19:08 +0200

    config update

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ from pathlib import Path
 from typing import Literal
 
 class logging:
-    root: Path = Path(__file__).parent/'logs'/'test'
+    root: Path = Path(__file__).parent/'logs'/'prod'
 
 class models:
     batch_size = 1000
 
 class storage:
     type loc = Literal['folder', 'db', 'mem']
-    location: loc = 'folder'
-    folder_path: Path = Path(__file__).parent/'storage'/'test'
-    db_path: Path = Path(__file__).parent/'storage'/'test.db'
+    location: loc = 'db'
+    folder_path: Path = Path(__file__).parent/'storage'/'prod'
+    db_path: Path = Path(__file__).parent/'storage'/'prod.db'
 
 class providers:
     live_time_frame: float = 5*24*3600.0
