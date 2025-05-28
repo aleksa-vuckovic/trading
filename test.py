5ab3e6e43b9fbc03b346384ac7f2f2a56994a559
Aleksa Vuckovic <aleksa.vuckovic36@gmail.com>
2025-05-28 22:23:53 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ config.logging.root = "logs/test"
 config.storage.local_root_path = "storage/test"
 config.storage.local_db_path = "storage/test.db"
 config.storage.mongo_db_name = "trading_test"
-config.http.request_log = "long"
-config.http.response_log = "long"
-from trading import log
+config.http.request_log = "short"
+config.http.response_log = "short"
+import log
 log.configure_logging()
 
-start_dir = "base" if len(sys.argv) < 2 else sys.argv[1]
+start_dir = "." if len(sys.argv) < 2 else sys.argv[1]
 loader = unittest.TestLoader()
 suite = loader.discover(start_dir=start_dir, pattern="test_*.py", top_level_dir=".")
 
