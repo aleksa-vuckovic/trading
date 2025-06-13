22de49c125167347afb3c24880b039f92687f2d8
Aleksa Vuckovic <aleksa.vuckovic36@gmail.com>
2025-06-13 17:04:53 +0200

    Enable test running for single files

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+from pathlib import Path
 import config
 config.logging.root = "logs/test"
 config.storage.local_root_path = "storage/test"
@@ -10,9 +11,19 @@ config.http.response_log = "short"
 import log
 log.configure_logging()
 
-start_dir = "." if len(sys.argv) < 2 else sys.argv[1]
-loader = unittest.TestLoader()
-suite = loader.discover(start_dir=start_dir, pattern="test_*.py", top_level_dir=".")
+__import__('sys').modules['unittest.util']._MAX_LENGTH = 999999999
 
+default_dir = "base/tests/serialization"
+start_dir = Path(default_dir if len(sys.argv) < 2 else sys.argv[1])
+if start_dir.is_dir():
+    pattern = "test_*.py"
+elif start_dir.parent.is_dir():
+    pattern = f"*{start_dir.name}*"
+    start_dir = start_dir.parent
+else:
+    raise Exception(f"Invalid start directory {start_dir}.")
+
+loader = unittest.TestLoader()
+suite = loader.discover(start_dir=str(start_dir), pattern=pattern, top_level_dir=".")
 runner = unittest.TextTestRunner()
 runner.run(suite)
